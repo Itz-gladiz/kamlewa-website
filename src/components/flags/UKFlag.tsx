aa544aae7f0d2174bb8c10481df83c6307aa3b71
unknown <serenaskillcraft@gmail.com>
2026-07-09 08:54:24 +0100

    Fixed UKflag on Navbar

--- src/components/flags/UKFlag.tsx
+++ src/components/flags/UKFlag.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import Image from 'next/image';
 
 interface UKFlagProps {
   className?: string;
 }
 
-export default function UKFlag({ className = "w-5 h-5" }: UKFlagProps) {
+export default function UKFlag({ className = 'w-5 h-5' }: UKFlagProps) {
   return (
-    <Image
-      src="https://img.freepik.com/free-vector/illustration-uk-flag_53876-18166.jpg"
-      alt="UK Flag"
-      width={20}
-      height={20}
+    <svg
+      viewBox="0 0 60 30"
       className={className}
-      style={{ objectFit: 'contain' }}
-    />
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-label="UK flag"
+    >
+      <rect width="60" height="30" fill="#012169" />
+      <path d="M0 0 L60 30 M60 0 L0 30" stroke="#fff" strokeWidth="6" />
+      <path d="M0 0 L60 30 M60 0 L0 30" stroke="#C8102E" strokeWidth="4" />
+      <rect x="25" y="0" width="10" height="30" fill="#fff" />
+      <rect x="27.5" y="0" width="5" height="30" fill="#C8102E" />
+      <rect x="0" y="10" width="60" height="10" fill="#fff" />
+      <rect x="0" y="11.5" width="60" height="7" fill="#C8102E" />
+    </svg>
   );
 }
